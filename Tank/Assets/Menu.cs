f36cad68a38beddc13be3f4e0bd74cb0d2a4be66
Anwar-bit <65156523+Anwar-bit@users.noreply.github.com>
2020-06-09 08:55:27 +0500

    done

--- Tank/Assets/Menu.cs
+++ Tank/Assets/Menu.cs
@@ -6,6 +6,6 @@ public class Menu : MonoBehaviour
 {
   public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
