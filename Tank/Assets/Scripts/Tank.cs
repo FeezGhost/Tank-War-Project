7a47a700e07128cf4bd345d982e56043f4e62738
Anwar-bit <65156523+Anwar-bit@users.noreply.github.com>
2020-06-11 07:12:19 +0500

    anwar

---
+++ Tank/Assets/Scripts/Tank.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : MonoBehaviour
+{
+    // private  controller= CharacterController;
+
+    /////*******************************************/////
+    /////                   VARS                    /////  
+    /////*******************************************/////
+
+
+}
+
+
+
+
+
+
